e6aa3a80ea86539b279e8e20dd9ba28cb703250b
Will Green <WillGreen@users.noreply.github.com>
2019-03-06 16:50:33 +0000

    Improve the TMDS model and include example output.

--- README.md
+++ README.md
@@ -94,15 +94,17 @@ There is also a _top_ module for the display controller, [demo](hdl/demo) versio
 ## TMDS Encoder Model
 The display controller includes a simple [Python model](model/tmds.py) to help with TMDS encoder development. 
 
-There are two steps to TMDS encoding: X(N)ORing the bit together to minimise transitions, and keeping the overall number of 1s and 0s similar to ensure DC balance. The first step depends only on the current input data, so is easy to test. However, balancing depends on the previous values, which makes testing harder. The Python model provides a point of reference and easy way to experiment with the encoder design.
+There are two steps to TMDS encoding: applying XOR or XNOR to the bits to minimise transitions, and keeping the overall number of 1s and 0s similar to ensure DC balance. The first step depends only on the current input value, so is easy to test. However, balancing depends on the previous values, which makes testing harder; this is where the model is particularly useful. 
 
-Python output:
+By default the Python model encodes all 256 possible 8-bit values in order, but it's easy to change the script to handle other combinations. `A0, A1, B0, or B1` show which of the four balancing options was taken: you can see what they do in the Python or Verilog code.
+
+Sample Python output:
 
     30: XNOR(2, 0, A1) [...] -> [0, 0, 0, 0, 0, 1, 0, 1, 0] -> [...]
     31: XNOR(6, 4, B1) [...] -> [1, 1, 1, 1, 1, 0, 1, 0, 0] -> [...]
     32: XOR (3, 0, A0) [...] -> [0, 0, 0, 0, 0, 1, 1, 1, 1] -> [...]
 
-Verilog output:
+Sample Verilog output:
 
     30 010100000   2,   0, A1
     31 001011111   6,   4, B1
--- model/tmds.py
+++ model/tmds.py
@@ -4,6 +4,8 @@
 # (C)2019 Will Green, Open source software released under the MIT License
 # Learn more at https://projectf.io
 
+# NB. [0:7] is 7 elements in Python, but 8 in Verilog.
+
 from __future__ import print_function
 
 def static_vars(**kwargs):
@@ -22,8 +24,8 @@ def bin_array_8(x):
 
 def tmds(decimal, d):
     '''
+    Convert d to q_m as per DVI spec.
     Perform base TMDS encoding. Does not balance.
-    Convert d[0:7] to q_m[0:8] as per DVI spec.
     '''
     q_m = [None] * 9
     one_cnt_d = sum(d)
@@ -54,8 +56,8 @@ def tmds(decimal, d):
 @static_vars(bias=0)
 def bias(q_m):
     '''
-    Perform TMDS bias encoding. Generates one of 460 10-bit outputs.
-    Convert q_m[0:8] to q_m[0:9] as per DVI spec.
+    Convert q_m to q_out as per DVI spec.
+    Perform TMDS balancing to handle bias. Generate one of 460 10-bit outputs.
     '''
     q_out = [None] * 10
     one_cnt = sum(q_m[0:8])
@@ -67,35 +69,34 @@ def bias(q_m):
         q_out[8] = q_m[8]
         if q_m[8] == 0:
             print("{},{:2}, A1) ".format(one_cnt, bias.bias), end='')
-            q_out[0:8] = [int(not i) for i in q_m[0:8]]
+            q_out[:8] = [int(not i) for i in q_m[:8]]  # inverted q_m[:8]
             bias.bias = bias.bias + zero_cnt - one_cnt
         else:
             print("{},{:2}, A0) ".format(one_cnt, bias.bias), end='')
-            q_out[0:8] = q_m[0:8]
+            q_out[:8] = q_m[:8]
             bias.bias = bias.bias + one_cnt - zero_cnt
     else:
         if (bias.bias > 0 and one_cnt > zero_cnt) or (bias.bias < 0 and one_cnt < zero_cnt):
             print("{},{:2}, B1) ".format(one_cnt, bias.bias), end='')
             q_out[9] = 1
             q_out[8] = q_m[8]
-            q_out[0:8] = [int(not i) for i in q_m[0:8]]
+            q_out[:8] = [int(not i) for i in q_m[:8]]  # inverted q_m[:8]
             bias.bias = bias.bias + 2 * q_m[8] + zero_cnt - one_cnt
         else:
             print("{},{:2}, B0) ".format(one_cnt, bias.bias), end='')
             q_out[9] = 0
             q_out[8] = q_m[8]
-            q_out[0:8] = q_m[0:8]
+            q_out[:8] = q_m[:8]
             bias.bias = bias.bias - 2 * (not q_m[8]) + one_cnt - zero_cnt
     return q_out
 
 
-print("Project F TMDS Encoding Model")
-print("================================")
-print("d[0:7] -> q_m[0:8] -> q_out[0:9]")
-print("LSB First, 8=X(N)OR  9=invert\n")
+print("Project F TMDS Encoding Model from https://projectf.io");
+print("d -> q_m -> q_out (LSB first) - 1s: one count, B: bias, O: balance option. 0-7: data, 8: X(N)OR, 9: inverted\n")
 print("         1s  B   O   0  1  2  3  4  5  6  7      0  1  2  3  4  5  6  7  8      0  1  2  3  4  5  6  7  8  9")
 print("=============================================================================================================")
-for decimal in range(0,256):
+
+for decimal in range(0,256):  # encode all possible 8-bit values (0-255)
     d = bin_array_8(decimal)
     q_m = tmds(decimal, d)
     q_out = bias(q_m)
