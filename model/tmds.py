bc02b884ec946a03bde1f1e70c41d1f8bbb8bb86
Will Green <WillGreen@users.noreply.github.com>
2019-04-06 16:00:46 +0100

    Output data MSB first to match Verilog and typical usage. Output data in compact format to match Verilog. Resolve Python style issues (PEP 8).

--- model/tmds.py
+++ model/tmds.py
@@ -1,36 +1,37 @@
 #!/usr/bin/env python
 
-# Project F: TMDS Encoder Python Model
+# Project F: HDMI/DVI TMDS Encoder Python Model
 # (C)2019 Will Green, Open source software released under the MIT License
 # Learn more at https://projectf.io
 
 # NB. [0:7] is 7 elements in Python, but 8 in Verilog.
 
+"""HDMI/DVI TMDS Encoder Python Model"""
+
 from __future__ import print_function
 
 def static_vars(**kwargs):
+    """Static variable decorator."""
     def decorate(func):
         for k in kwargs:
             setattr(func, k, kwargs[k])
         return func
     return decorate
 
-def bin_array_8(x):
-    '''
-    Convert integer into fixed-length 8-bit binary array. LSB in [0].
-    '''
-    b_array = [int(i) for i in reversed(bin(x)[2:])]
+def bin_array_8(integer):
+    """Convert integer into fixed-length 8-bit binary array. LSB in [0]."""
+    b_array = [int(i) for i in reversed(bin(integer)[2:])]
     return b_array + [0 for _ in range(8 - len(b_array))]
 
-def tmds(decimal, d):
-    '''
+def tmds(d, d_decimal):  # pylint: disable=invalid-name
+    """
     Convert d to q_m as per DVI spec.
     Perform base TMDS encoding. Does not balance.
-    '''
+    """
     q_m = [None] * 9
     one_cnt_d = sum(d)
     if one_cnt_d > 4 or (one_cnt_d == 4 and d[0] == 0):
-        print("{:3}: XNOR(".format(decimal), end='')
+        print("{:3}: XNOR(".format(d_decimal), end='')
         q_m[0] = d[0]
         q_m[1] = int(q_m[0] == d[1])
         q_m[2] = int(q_m[1] == d[2])
@@ -41,7 +42,7 @@ def tmds(decimal, d):
         q_m[7] = int(q_m[6] == d[7])
         q_m[8] = 0  # using XNOR == 0
     else:
-        print("{:3}: XOR (".format(decimal), end='')
+        print("{:3}: XOR (".format(d_decimal), end='')
         q_m[0] = d[0]
         q_m[1] = int(q_m[0] ^ d[1])
         q_m[2] = int(q_m[1] ^ d[2])
@@ -55,14 +56,13 @@ def tmds(decimal, d):
 
 @static_vars(bias=0)
 def bias(q_m):
-    '''
+    """
     Convert q_m to q_out as per DVI spec.
     Perform TMDS balancing to handle bias. Generate one of 460 10-bit outputs.
-    '''
+    """
     q_out = [None] * 10
     one_cnt = sum(q_m[0:8])
     zero_cnt = 8 - one_cnt
-    option = ''
 
     if bias.bias == 0 or one_cnt == 4:
         q_out[9] = int(not q_m[8])
@@ -90,14 +90,22 @@ def bias(q_m):
             bias.bias = bias.bias - 2 * (not q_m[8]) + one_cnt - zero_cnt
     return q_out
 
+def main():
+    """Generate formatted table of TMDS encoded values."""
+    print("Project F TMDS Encoding Model from https://projectf.io")
+    print("d -> q_m -> q_out (MSB first) - 1s: one count, B: bias")
+    print("O: balance option. 0-7: data, 8: X(N)OR, 9: inverted\n")
+    print("         1s  B   O  76543210    876543210    9876543210")
+    print("=======================================================")
 
-print("Project F TMDS Encoding Model from https://projectf.io");
-print("d -> q_m -> q_out (LSB first) - 1s: one count, B: bias, O: balance option. 0-7: data, 8: X(N)OR, 9: inverted\n")
-print("         1s  B   O   0  1  2  3  4  5  6  7      0  1  2  3  4  5  6  7  8      0  1  2  3  4  5  6  7  8  9")
-print("=============================================================================================================")
+    for decimal in range(0, 256):  # encode all possible 8-bit values (0-255)
+        d = bin_array_8(decimal)   # pylint: disable=invalid-name
+        q_m = tmds(d, decimal)
+        q_out = bias(q_m)
+        d_str = ''.join(map(str, reversed(d)))
+        q_m_str = ''.join(map(str, reversed(q_m)))
+        q_out_str = ''.join(map(str, reversed(q_out)))
+        print("{} -> {} -> {}".format(d_str, q_m_str, q_out_str))
 
-for decimal in range(0,256):  # encode all possible 8-bit values (0-255)
-    d = bin_array_8(decimal)
-    q_m = tmds(decimal, d)
-    q_out = bias(q_m)
-    print("{} -> {} -> {}".format(d, q_m, q_out))
+if __name__ == "__main__":
+    main()
